cb2bf15dd562034fdaf9508d615e4009253e697b
mamulee <ghdah7@gmail.com>
2023-12-16 22:37:46 +0900

    create Memo

--- lib/main.dart
+++ lib/main.dart
@@ -85,6 +85,15 @@ class _HomePageState extends State<HomePage> {
             child: Icon(Icons.add),
             onPressed: () {
               // + 버튼 클릭시 메모 생성 및 수정 페이지로 이동
+              memoService.createMemo(content: '');
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => DetailPage(
+                    index: memoService.memoList.length - 1,
+                  ),
+                ),
+              );
             },
           ),
         );
